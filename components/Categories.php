f807577871bb0a9a9e4e41c277de0c8f24173c7a
Ruslan Puryaev <ruslan-cool@live.ru>
2016-09-23 12:20:07 +0700

    Create component categories

---
+++ components/Categories.php
@@ -0,0 +1,21 @@
+<?php namespace Queni\DLearning\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Categories extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'categories Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
