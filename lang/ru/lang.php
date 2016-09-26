c3763dd03f6cb15478fba86f6ba9a4891d4b3207
Aleksei Fedorov <aleksei_fedorov@outlook.com>
2016-09-26 13:34:14 +0700

    Add folder locale ru

---
+++ lang/ru/lang.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Дистанционное обучение',
+        'description' => 'Система управления обучением для OctoberCMS.',
+        'authors' => 'Руслан Пуряев, Алексей Фёдоров'
+    ],
+    'dlearning' => [
+        'info_course' => 'Информация о курсе',
+        'authors' => 'Авторы',
+        'access_to_course' => 'Доступ к курсу',
+        'date' => 'Дата',
+        'id' => 'Идентификатор',
+        'title' => 'Заголовок',
+        'slug' => 'Шифр',
+        'description' => 'Описание',
+        'is_published' => 'Опубликовать',
+        'is_published_course_comment' => 'Выберите для публикации курса',
+        'category' => 'Категория',
+        'name_plugin' => 'Дистанционное обучение',
+        'course' => 'Курс',
+        'courses' => 'Курсы',
+        'lessons' => 'Лекции',
+        'categories' => 'Категории',
+        'new_course' => 'Добавить курс',
+        'return_course_list' => 'Вернуться к списку курсов',
+        'cancel' => 'Отмена',
+        'or' => 'или',
+        'create' => 'Создать',
+        'create_and_close' => 'Создать и Закрыть'
+    ]
+];
