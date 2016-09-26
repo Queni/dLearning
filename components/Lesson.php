2ba1372b740917faf861dff5611c26898822979f
Ruslan Puryaev <ruslan-cool@live.ru>
2016-09-27 02:01:32 +0700

    Create component lesson

---
+++ components/Lesson.php
@@ -0,0 +1,21 @@
+<?php namespace Queni\DLearning\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Lesson extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Lesson Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
