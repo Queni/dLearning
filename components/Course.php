83620173560bd570a6a7702da53ccec1e8a5c9e4
Ruslan Puryaev <ruslan-cool@live.ru>
2016-09-27 01:42:07 +0700

    Create component course

---
+++ components/Course.php
@@ -0,0 +1,37 @@
+<?php namespace Queni\DLearning\Components;
+
+use Cms\Classes\ComponentBase;
+use Queni\DLearning\Models\Courses as CoursesModel;
+
+class Course extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Course Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'currentCourse' => [
+                'title'             => 'Current category',
+                'description'       => 'Current category to display',
+                'default'           => '{{ :course }}'
+            ]
+        ];
+    }
+
+    public function info()
+    {
+        $currentCourse = $this->param('currentCourse');
+
+        if ($currentCourse == null) return null;
+
+        return CoursesModel::where('slug', $currentCourse)->get();
+    }
+
+}
